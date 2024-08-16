d78ffb5d79a7a0cf3873a0e7441d6b4d1e3118ac
Saba Ahmed <ahmedsaba271@gmail.com>
2024-08-16 12:45:22 -0400

    Update DeckActions.java

--- src/main/java/org/example/DeckActions.java
+++ src/main/java/org/example/DeckActions.java
@@ -3,6 +3,13 @@ package org.example;
 import java.util.ArrayList;
 
 public interface DeckActions {
+     public void shuffle() ;
+
+    public Card dealNextCard();
+
+    public void printDeck(int numToPrint) ;
+
     void playHand(ArrayList<Card> hand);
     void determineWinner();
+}
 }
