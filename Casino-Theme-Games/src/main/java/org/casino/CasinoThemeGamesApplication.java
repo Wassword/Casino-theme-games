2059c4fbee74edd8496ab88b687d1a391a106677
TylerJac <sans.gaming235@gmail.com>
2024-10-17 15:15:57 -0400

    adding spring boot

---
+++ Casino-Theme-Games/src/main/java/org/casino/CasinoThemeGamesApplication.java
@@ -0,0 +1,13 @@
+package org.casino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasinoThemeGamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasinoThemeGamesApplication.class, args);
+	}
+
+}
---
+++ Casino-Theme-Games/src/test/java/org/casino/CasinoThemeGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.casino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasinoThemeGamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
